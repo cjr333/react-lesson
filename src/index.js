00db0e7202570c32011d4680742dedb0fb4ef9ec
cjr333 <25564688+cjr333@users.noreply.github.com>
2018-01-22 09:18:05 +0900

    YTSearch 를 이용한 데이터 패치

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar'
 
 const API_KEY = "AIzaSyCZ0FzBGlP8vSCX5b9NxfveF2JLkV-zc0k";
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function (data) {
+  console.log(data);
+});
 
 // Create a new component. This component should produce
 // some HTML
