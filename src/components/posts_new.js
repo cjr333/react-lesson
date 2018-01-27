5092eaaa75bca9a7dd84451a9abd0289f65ab4fe
cjr333 <25564688+cjr333@users.noreply.github.com>
2018-01-27 23:30:41 +0900

    form submit 구현

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
-import { reduxForm }
+import { reduxForm } from 'redux-form';
+import { createPost } from "../actions/index";
 
 class PostsNew extends Component {
   render() {
+    const { fields: { title, categories, content }, handleSubmit } = this.props;
+
     return (
-      <div>Create form</div>
+      <form onSubmit={handleSubmit(this.props.createPost)}>
+        <h3>Create A New Post</h3>
+        <div className="form-group">
+          <label>Title</label>
+          <input type="text" className="form-control" {...title} />
+          <label>Categories</label>
+          <input type="text" className="form-control" {...categories} />
+          <label>Content</label>
+          <textarea className="form-control" {...content} />
+        </div>
+
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
     );
   }
 }
 
-export default PostsNew;
+// connect: first argument is mapStateToProps, 2nd is mapDispatchToProps
+// reduxForm: 1st is form config, 2nd is mapStateToProps, 3rd is mapDispatchToProps
+
+export default reduxForm({
+  form: "PostsNewForm",
+  fields: ["title", "categories", "content"]
+}, null, { createPost })(PostsNew);
