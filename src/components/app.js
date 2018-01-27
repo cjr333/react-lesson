6173cf31c510c2b0ac280a8a3aa5d5ff4eabc472
cjr333 <25564688+cjr333@users.noreply.github.com>
2018-01-27 21:44:20 +0900

    react-router 연결... 강의와는 다르게 react-router-redux 를 통해서 연결... github page 참조

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import PostsIndex from './posts_index';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <PostsIndex></PostsIndex>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,14 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
-import App from './components/app';
+import createHistory from 'history/createBrowserHistory'
+import { Route, IndexRoute } from 'react-router';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+import App from './components/app';
+
+import { ConnectedRouter, routerReducer, routerMiddleware, push } from 'react-router-redux'
+
+// http://www.blog.com/posts/5 -- browserHistory
+// http://www.blog.com/posts/5# -- hashHistory
+// memoryHistory - 잘 사용하지 않음
+
+// Create a history of your choosing (we're using a browser history in this case)
+const history = createHistory()
+
+const createStoreWithMiddleware = applyMiddleware(routerMiddleware(history))(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <ConnectedRouter history={history}>
+      <Route exact path="/" component={App}>
+      </Route>
+    </ConnectedRouter>
   </Provider>
   , document.querySelector('.container'));
