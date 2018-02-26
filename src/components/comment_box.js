81c42089b1d032f7984cc43159a72821f19c7f24
cjr333 <cjr333@nexon.co.kr>
2018-02-26 22:43:51 +0900

    Reducer 작성 및 test 작성 완료.. App 종료

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
-export default class CommentBox extends Component {
+class CommentBox extends Component {
   constructor(props) {
     super(props);
 
@@ -14,15 +16,21 @@ export default class CommentBox extends Component {
   handleSubit(event) {
     event.preventDefault();
 
+    this.props.saveComment(this.state.comment);
     this.setState({ comment: ''});
   }
 
   render() {
     return (
       <form className="comment-box" onSubmit={this.handleSubit.bind(this)}>
+        <h4>Add a comment</h4>
 		    <textarea value={this.state.comment} onChange={this.handleChange.bind(this)} />
-        <button action="submit">Submit Comment</button>
+        <div>
+          <button action="submit">Submit Comment</button>
+        </div>
       </form>
     );
   }
 }
+
+export default connect(null, actions)(CommentBox);
