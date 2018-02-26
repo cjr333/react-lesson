fdfaeaaf1d6a112e6013b9439b4c7c32e8ec8368
cjr333 <25564688+cjr333@users.noreply.github.com>
2018-02-26 21:22:01 +0900

    Comment box spec 완료

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 
 export default class CommentBox extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { comment: '' };
+  }
+
+  handleChange(event) {
+    this.setState({ comment: event.target.value });
+  }
+
+  handleSubit(event) {
+    event.preventDefault();
+
+    this.setState({ comment: ''});
+  }
+
   render() {
     return (
-      <div className="comment-box">
-		    <textarea />
-        <button>Submit Comment</button>
-      </div>
+      <form className="comment-box" onSubmit={this.handleSubit.bind(this)}>
+		    <textarea value={this.state.comment} onChange={this.handleChange.bind(this)} />
+        <button action="submit">Submit Comment</button>
+      </form>
     );
   }
 }
